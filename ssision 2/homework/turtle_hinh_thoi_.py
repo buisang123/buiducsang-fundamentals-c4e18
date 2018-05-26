2f6863036415e2dc4eed1879f03eb89cbcb9b3bc
buisang123 <bducsang123@gmail.com>
2018-05-26 19:50:25 +0700

    fix tutle

--- ssision 2/homework/turtle_hinh_thoi_.py
+++ ssision 2/homework/turtle_hinh_thoi_.py
@@ -2,42 +2,17 @@ from turtle import *
 shape("turtle")
 color("red")
 
-left(30)
-forward(100)
-right(60)
-forward(100)
-right(120)
-forward(100)
-right(60)
-forward(100)
-
-right(90)
-forward(100)
-left(60)
-forward(100)
-left(120)
-forward(100)
-left(60)
-forward(100)
-
-right(150)
-forward(100)
-left(60)
-forward(100)
-left(120)
-forward(100)
-left(60)
-forward(100)
-
-right(150)
-forward(100)
-left(60)
-forward(100)
-left(120)
-forward(100)
-left(60)
-forward(100)
-
+for i in range(4):
+    left(30)
+    forward(100)
+    right(60)
+    forward(100)
+    right(120)
+    forward(100)
+    right(60)
+    forward(100)
+    right(60)
 
 mainloop()
+
 
